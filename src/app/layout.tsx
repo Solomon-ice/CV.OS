49d2ae804575d1dd789381d57c2aa9009c3cf27f
solomonprincebright@gmail.com <solomonprincebright@gmail.com>
2026-03-20 02:24:10 +0000

    add google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import Navbar from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CV.OS | Premium Resume Builder",
-  description: "Design a cinematic, high-end resume with ease.",
+  title: "Solomon and Stephen | Premium Resume Builder",
+  description:  "Create professional resumes online with modern templates and instant PDF download.",
 };
 
 export default function RootLayout({
